0a26894e46b6d1f419afcdc7a5ca36fdc9ee8de1
Jeff Smith <jeffrey.k.smith@thomsonreuters.com>
2024-02-07 17:43:57 +0000

    Simplified code

--- zero/weatherstation.py
+++ zero/weatherstation.py
@@ -8,28 +8,17 @@ bus = smbus2.SMBus(1)
 
 calibration_params = bme280.load_calibration_params(bus,address)
 
-def celcius_to_fahrenheit(celcius):
-        return (celcius * 9/5) + 32
-
 def main():
         while True:
-                try:
-                        data = bme280.sample(bus, address, calibration_params)
-                        temperature_celcius = data.temperature
-                        pressure = data.pressure
-                        humidity = data.humidity
-                        temperature_fahrenheit = celcius_to_fahrenheit(temperature_celcius)
+                data = bme280.sample(bus, address, calibration_params)
+                temperature_celcius = data.temperature
+                pressure = data.pressure
+                humidity = data.humidity
 
-                        print('Temperature {:.2f}°C, {:.2f}°F'.format(temperature_celcius, temperature_fahrenheit))
-                        print('Pressure {:.2f} hPa'.format(pressure))
-                        print('Humidity: {:.2f}%'.format(humidity))
-                        time.sleep(2)
-                except KeyboardInterrupt:
-                        print('Stopped')
-                        break
-                except Exception as e:
-                        print('An unexpected error occurred: ', str(e))
-                        break
+                print(f'Temperature {temperature_celcius:.2f}°C')
+                print(f'Pressure {pressure:.2f} hPa')
+                print(f'Humidity: {humidity:.2f}%')
+                time.sleep(2)
 
 if __name__ == "__main__":
         main()
