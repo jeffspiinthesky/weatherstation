7c8e9ef5c4b98ce49b9b71b2e95e824a21e5e578
jeffspiinthesky <jeffspiinthesky@gmail.com>
2024-05-20 08:53:54 +0100

    Put Wifi connection in try/except

--- pico/Wifi.py
+++ pico/Wifi.py
@@ -22,23 +22,27 @@ class Wifi:
         # Continually loop until a connection is established
         while True:
             for credentials in self.data['Credentials']:
-                print(f'Attempting to connect to *********')
-                retry_count = 0
-                self.wlan = WLAN(STA_IF)
-                self.wlan.active(True)
-                self.wlan.connect(credentials['SSID'], credentials['Password'])
-                while (self.wlan.isconnected() == False) and (retry_count < 10):
-                    print(f'Waiting for connection...Attempt {retry_count}')
-                    retry_count = retry_count + 1
-                    sleep(5)
-                if retry_count == 10:
-                    print(f'Cannot connect to {credentials['SSID']} - retries exceeded')
-                else:
-                    ip = self.wlan.ifconfig()[0]
-                    print(f'Connected on {ip}')
-                    self.led.on()
-                    state = 'ON'
-                    return ip
+                try:
+                    #print(f'Attempting to connect to *********')
+                    retry_count = 0
+                    self.wlan = WLAN(STA_IF)
+                    self.wlan.active(True)
+                    self.wlan.connect(credentials['SSID'], credentials['Password'])
+                    while (self.wlan.isconnected() == False) and (retry_count < 10):
+                        print(f'Waiting for connection...Attempt {retry_count}')
+                        retry_count = retry_count + 1
+                        sleep(5)
+                    if retry_count == 10:
+                        print(f'Cannot connect to {credentials['SSID']} - retries exceeded')
+                    else:
+                        ip = self.wlan.ifconfig()[0]
+                        print(f'Connected on {ip}')
+                        self.led.on()
+                        state = 'ON'
+                        return ip
+                except OSError as error:
+                    print(f'Error: {error}')
+                    sleep(1)
                 
     def disconnect(self):
         if ( self.wlan.isconnected() ):
